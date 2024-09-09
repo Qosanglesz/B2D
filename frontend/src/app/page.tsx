f4752ed0fd7ea053f398f3e8dcaeedf8af45143f
Chaiyawut Thengket <santa081831@gmail.com>
2024-09-10 02:09:27 +0700

    create landing page for the first page when user come in website

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,18 @@
-import Header from "@/components/Header";
-import Campaign from "@/components/Campaign";
-import React from "react";
+import Head from 'next/head';
+import HeroSection from '../components/HeroSection';
 
 export default function Home() {
-  return(
-      <>
-        <div>Home</div>
-      </>
+  return (
+    <>
+      <Head>
+        <title>B2D Venture - Your Startup Investment Platform</title>
+        <meta name="description" content="Invest in the next big startup or bring your business idea to life with B2D Venture." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <HeroSection />
+        {/* Add other sections of your landing page here */}
+      </main>
+    </>
   );
 }
