7f52a8d90c6c6fbf47bfbff167eb4bd674c4a6d7
Chaiyawut Thengket <santa081831@gmail.com>
2024-10-05 22:09:56 +0700

    fix

--- frontend/src/app/api/auth/[auth0]/route.tsx
+++ frontend/src/app/api/auth/[auth0]/route.tsx
@@ -1,14 +1,4 @@
 // app/api/auth/[auth0]/route.js
 import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
 
-export const GET = handleAuth({
-    login: handleLogin (
-        {
-            authorizationParams: {
-                audience : process.env.AUTH0_AUDIENCE,
-                // connection : 'google-oauth2',
-            },
-            returnTo: '/profile',
-        }
-    )
-});
+export const GET = handleAuth();
