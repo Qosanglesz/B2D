2b735214fb0473db835419a0c56e66573c928b71
MeHappyLucky <menggy2546@gmail.com>
2024-10-15 01:33:19 +0700

    Make landing page redirect to home

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -24,52 +24,29 @@
 
 "use client"; // Indicate this is a client component
 
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useRouter } from 'next/navigation'; // Use 'next/navigation' for Next.js 13 and above
-import Head from 'next/head';
-import HeroSection from '../components/landingPageComponent/HeroSection';
 import { Spinner } from '@nextui-org/react';
-import NavBar from '@/components/navAndFooter/NavBar';
-import { LoadingError } from '../components/landingPageComponent/Loading'; // Import the LoadingError component
+
 
 export default function Home() {
     const router = useRouter();
-    const [loading, setLoading] = useState(true); // Loading state
 
+    // Redirect to /home
     useEffect(() => {
-        const timer = setTimeout(() => {
-            // Redirect to /home after a short delay
-            router.push('/home');
-            setLoading(false); // Set loading to false after redirect
-        }, 100); // Adjust the delay as needed
-
-        return () => clearTimeout(timer); // Cleanup timer on unmount
+        // Redirect immediately
+        router.push('/home');
     }, [router]);
 
+    // Show loading spinner
     return (
-        <>
-            <Head>
-                <title>B2D Venture - Your Startup Investment Platform</title>
-                <meta name="description" content="Invest in the next big startup or bring your business idea to life with B2D Venture." />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-
-            {/* Show LoadingError component while waiting for the redirect */}
-            <LoadingError loading={loading} />
-            
-            {/* Main content can still be rendered if needed */}
-            {!loading && (
-                <main>
-                  <div className="flex flex-col justify-center items-center h-screen">
-                    <Spinner size="lg" />
-
-                    {/* Loading text added here */}
-                    <span className="mt-4 text-lg text-gray-400">
-                      Loading, please wait...
-                    </span>
-                  </div>
-                </main>
-            )}
-        </>
+        <div className="flex flex-col justify-center items-center h-screen">
+            <Spinner size="lg" />
+            {/* Loading text */}
+            <span className="mt-4 text-lg text-gray-400">
+                Loading, please wait...
+            </span>
+        </div>
     );
 }
+
