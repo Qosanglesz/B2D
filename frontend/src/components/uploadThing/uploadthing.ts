3ce7dbbdb8c96cd379b62d4d8c77357828e7a692
Wissarut Kanasub <wissarut.dev@gmail.com>
2024-10-16 14:31:01 +0700

    Update uploadthing.ts

--- frontend/src/components/uploadThing/uploadthing.ts
+++ frontend/src/components/uploadThing/uploadthing.ts
@@ -2,6 +2,9 @@ import {
     generateUploadButton,
     generateUploadDropzone,
 } from "@uploadthing/react";
-import type { OurFileRouter } from "@/controller/uploadThingAPI/uploadThingController";
+
+import type {OurFileRouter} from "@/controller/uploadThingAPI/uploadThingController";
+
+
 export const UploadButton = generateUploadButton<OurFileRouter>();
 export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
