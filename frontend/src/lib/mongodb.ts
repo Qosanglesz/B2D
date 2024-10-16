cf7a8200410a243d4a456de04a13d78ebad48165
Wissarut Kanasub <wissarut.dev@gmail.com>
2024-10-16 14:20:25 +0700

    Update mongodb.ts

--- frontend/src/lib/mongodb.ts
+++ frontend/src/lib/mongodb.ts
@@ -1,18 +1,18 @@
-import { MongoClient, ServerApiVersion } from 'mongodb';
+import {MongoClient, ServerApiVersion} from 'mongodb';
 
 
 const url = process.env.MONGODB_URL;
 
 if (!url) {
-  throw new Error('MONGODB_URI is not defined in the environment variables');
+    throw new Error('MONGODB_URI is not defined in the environment variables');
 }
 
 const client = new MongoClient(url, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 let clientPromise: Promise<MongoClient>;
