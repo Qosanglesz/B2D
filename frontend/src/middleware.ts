08816626037bb405795e06546e256fccfaad8c0a
Wissarut Kanasub <wissarut.dev@gmail.com>
2024-10-23 20:09:38 +0700

    Update middleware to protect Sensitive APIs Endpoints

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -16,10 +16,45 @@ export default withMiddlewareAuthRequired(async (req: NextRequest) => {
         return NextResponse.redirect("/api/auth/login");
     }
 
-    // Check if the user has the 'Admin' role when trying to access the admin path
     const roles = session.user['https://localhost:3000/roles'] || [];
+    const uid = session.user.sub
+
+    // Check if the user has the 'Admin' role when trying to access the admin path
     if (req.nextUrl.pathname.startsWith('/admin') && !roles.includes('Admin B2D')) {
+        return NextResponse.redirect(new URL('/home', req.url));
+    }
+
+    // Protect Common User try to access others account data
+    const userIdFromPath = req.nextUrl.pathname.split('/')[3];
+    const decodedUserIdFromPath = decodeURIComponent(userIdFromPath);
+    if (
+        req.nextUrl.pathname.startsWith('/api/user/') &&
+        !req.nextUrl.pathname.endsWith('/patch') &&
+        decodedUserIdFromPath !== uid &&
+        !roles.includes('Admin B2D')
+    ) {
         return NextResponse.redirect(new URL('/home', req.url)); // Redirect to an unauthorized page
+    }
+
+    // Protect Common User view Others' Statements
+    const userIdStatement = req.nextUrl.pathname.split('/')[4];
+    const decodedUidToGetStatement = decodeURIComponent(userIdStatement);
+    if (
+        req.nextUrl.pathname.startsWith('/api/statement/byuserid/') &&
+        decodedUidToGetStatement !== uid &&
+        !roles.includes('Admin B2D')
+    ) {
+        return NextResponse.redirect(new URL('/home', req.url));
+    }
+
+    // Protect Common User try to access all Statements
+    if (req.nextUrl.pathname.startsWith('/api/statements') && !roles.includes('Admin B2D')) {
+        return NextResponse.redirect(new URL('/home', req.url));
+    }
+
+    // Protect Common User try to access all Users' data
+    if (req.nextUrl.pathname.startsWith('/api/users') && !roles.includes('Admin B2D')) {
+        return NextResponse.redirect(new URL('/home', req.url));
     }
 
     if (session.accessToken) {
@@ -30,5 +65,17 @@ export default withMiddlewareAuthRequired(async (req: NextRequest) => {
 });
 
 export const config = {
-    matcher: ["/admin/:path*", "/campaign/:path*", '/profile/:path*', '/portfolio/:path*'],
+    matcher: [
+        // Pages Section
+        "/campaign/:path*",
+        "/profile/:path*",
+        "/portfolio/:path*",
+        "/admin/:path*",
+        //APIs Section
+        "/api/users",
+        "/api/user/:path*",
+        "/api/statements",
+        "/api/statement/:path*"
+
+    ],
 };
