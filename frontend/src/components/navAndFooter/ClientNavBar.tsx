09d119624b88b69eab4fa01abd686c12d7a3a46b
Wissarut Kanasub <wissarut.dev@gmail.com>
2024-11-06 13:08:54 +0700

    remove search box in navbar

--- frontend/src/components/navAndFooter/ClientNavBar.tsx
+++ frontend/src/components/navAndFooter/ClientNavBar.tsx
@@ -25,17 +25,9 @@ const ClientNavBar: React.FC<ClientNavBarProps> = ({name, isAuth, links, user}) 
     return (
         <nav className="bg-gray-800 fixed py-4 top-0 w-full z-10">
 
-            <div className="max-w-7xl px-4 mx-auto flex items-center justify-between h-full">
+            <div className="max-w-6xl px-4 mx-auto flex items-center justify-between h-full">
                 <div className="text-white text-lg font-bold">
                     <a href="/home">{name}</a>
-                </div>
-
-                <div className="flex-1 mx-4">
-                    <input
-                        type="text"
-                        placeholder="Search..."
-                        className="bg-gray-700 text-white px-4 py-2 rounded-md w-full focus:outline-none focus:ring focus:border-blue-500"
-                    />
                 </div>
 
                 <div className="hidden md:flex items-center space-x-4">
