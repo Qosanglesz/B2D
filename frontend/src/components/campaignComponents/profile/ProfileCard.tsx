695257fa8baa086ffb0fc982ccc8cdfd28540be6
Wissarut Kanasub <wissarut.dev@gmail.com>
2024-11-18 11:34:46 +0700

    Update name nickname features secured

--- frontend/src/components/campaignComponents/profile/ProfileCard.tsx
+++ frontend/src/components/campaignComponents/profile/ProfileCard.tsx
@@ -1,23 +1,63 @@
+"use client"
+
 import Image from 'next/image';
 import { UserProfile } from '@auth0/nextjs-auth0/client';
+import {useState} from "react";
+import {EditProfileForm} from "@/components/campaignComponents/profile/EditProfileForm";
+import {useRouter} from "next/navigation";
 
 interface ProfileCardProps {
     user: UserProfile | undefined;
-    name: string | undefined;
-    nickname: string | undefined;
-    isEditing: boolean;
-    toggleEdit: () => void;
-    EditForm: React.ReactNode;
 }
 
-export const ProfileCard = ({ 
-    user, 
-    name, 
-    nickname, 
-    isEditing, 
-    toggleEdit,
-    EditForm 
+export const ProfileCard = ({
+    user,
 }: ProfileCardProps) => {
+    const router = useRouter();
+
+    const [isEditing, setIsEditing] = useState(false);
+    const [name, setName] = useState<string | undefined>(user?.name);
+    const [nickname, setNickname] = useState<string | undefined>(user?.nickname);
+    const toggleEdit = () => setIsEditing(!isEditing);
+
+    const handleSave = async () => {
+        try {
+            const tokenResponse = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/accesstoken`, {
+                method: "GET",
+                headers: {
+                    accesstokenapikey: process.env.NEXT_PUBLIC_ACCESS_TOKEN_API_KEY || "",
+                }
+            })
+            const tokenData = await tokenResponse.json()
+
+            const newData = { name, nickname };
+            const response = await fetch('/api/user/patch', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    authorization: `Bearer ${tokenData.access_token}`,
+
+                },
+                body: JSON.stringify({
+                    user_id: user?.sub,
+                    newData,
+                }),
+            });
+
+            if (!response.ok) {
+                const errorData = await response.json();
+                throw new Error(errorData.message || 'Failed to update user data');
+            }
+
+            setIsEditing(false);
+            alert('User information saved! Please login again to see changes');
+            router.push("/api/auth/logout");
+        } catch (error) {
+            console.error('Error saving user data:', error);
+            alert('Failed to save user information. Please try again.');
+        }
+    };
+
     return (
         <div className="bg-white p-4 rounded-lg shadow">
             <div className="mx-auto bg-white shadow-md rounded-lg p-4 sm:p-6">
@@ -34,7 +74,7 @@ export const ProfileCard = ({
 
                     <div className="flex-grow text-center sm:text-left">
                         {isEditing ? (
-                            EditForm
+                            <EditProfileForm name={name} nickname={nickname} setName={setName} setNickname={setNickname} handleSave={handleSave}/>
                         ) : (
                             <>
                                 <h2 className="text-xl sm:text-2xl font-semibold text-gray-800">{name}</h2>
